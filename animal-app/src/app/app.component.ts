917c5c510f19c94cf925c8edabb86af4777eb2c2
madhusudhanan-jayaram <madhusudhanan.jayaram.com>
2025-08-05 21:23:43 -0500

    feat: updated code

--- animal-app/src/app/app.component.ts
+++ animal-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from "./app-routing.module";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [AppRoutingModule],
 })
 export class AppComponent {
   title = 'animal-app';
