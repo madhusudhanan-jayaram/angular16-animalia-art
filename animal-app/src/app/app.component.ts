40434027ced5996d95b7bd59f178d243b31585f5
madhusudhanan-jayaram <madhusudhanan.jayaram.com>
2025-08-06 09:15:11 -0500

    feat: added routing logic

--- animal-app/src/app/app.component.ts
+++ animal-app/src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { AppRoutingModule } from "./app-routing.module";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  imports: [AppRoutingModule],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'animal-app';
